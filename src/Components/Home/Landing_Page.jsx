991bbf7a0b56e44ce84de4f005930022d03bafea
Rahul Jha <rjrahuljha02@gmail.com>
2023-04-23 15:02:38 +0530

    ss

---
+++ src/Components/Home/Landing_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import '../Home/landing-page.css'
+
+const Landing_Page = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Landing_Page;
