c1609976d3cda38984da4c407eb9ce5108b9a215
Rahul Jha <rjrahuljha02@gmail.com>
2023-04-24 19:02:19 +0530

    landingpage commit

--- src/Components/Home/Landing_Page.jsx
+++ src/Components/Home/Landing_Page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import '../Home/landing-page.css'
 
 const Landing_Page = () => {
   return (
-    <div></div>
+    <section className='landing-page'>
+      <p>Welcome to The Milky Way's world. <br /> Here you can buy all your daily needs related to milk.</p>
+    </section>
   )
 }
 
