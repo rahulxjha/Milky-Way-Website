44da800b13e7abb73fa40a04d32d72e87988529d
Rahul Jha <rjrahuljha02@gmail.com>
2023-04-25 20:04:53 +0530

    hey

--- src/Components/Home/Landing_Page.jsx
+++ src/Components/Home/Landing_Page.jsx
@@ -4,7 +4,7 @@ import '../Home/landing-page.css'
 const Landing_Page = () => {
   return (
     <section className='landing-page'>
-      <p>Welcome to The Milky Way's world. <br /> Here you can buy all your daily needs related to milk.</p>
+      <p>Welcome to The Milky Way's world. <br /> Here you can buy all your daily milk needs.</p>
     </section>
   )
 }
