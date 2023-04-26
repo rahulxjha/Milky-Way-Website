cb4b527708e2bd7428bf15f8f6ca6ab0e10de35d
Rahul Jha <rjrahuljha02@gmail.com>
2023-04-26 21:23:43 +0530

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Home/Landing_Page';
 import Products from './Components/Products/Products';
+import Contact from './Components/Contact/Contact';
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
         <Navbar />
         <Landing_Page />
         <Products />
+        <Contact />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../Contact/contact.css';
 
 const Contact = () => {
   return (
-    <div></div>
+    <section id='contact'>
+      <h1>Contact us</h1>
+      <div id="contact-box">
+        <form action="" method="get">
+          <fieldset>
+            <div className="form-group">
+              <label htmlFor="">Name:</label><br /><input type="text" placeholder="Enter your name"></input><br />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Email:</label><br /><input type="text" placeholder="Enter your email"></input><br />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Tell us:</label><br /><textarea name="" id="" cols="30" rows="10"></textarea><br />
+            </div>
+            <button id='contact-btn'>Submit</button>
+          </fieldset>
+        </form>
+      </div>
+    </section>
   )
 }
 
