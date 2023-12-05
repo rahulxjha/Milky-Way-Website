a9aa6077c6979c0eabdee9c491f3ebeb81e1abbe
Rahul Jha <rahul.jha@qtsolv.com>
2023-12-05 17:32:37 +0530

    made changes in css and components

--- src/Components/Home/Landing_Page.jsx
+++ src/Components/Home/Landing_Page.jsx
@@ -4,7 +4,7 @@ import '../Home/landing-page.css'
 const Landing_Page = () => {
   return (
     <section id='landing-page'>
-      <p>Welcome to The Milky Way's world. <br /> Here you can buy all your daily milk needs.</p>
+      <p>Welcome to The Milky Way's world. <br /> Here you can buy your all daily milk needs.</p>
     </section>
   )
 }
