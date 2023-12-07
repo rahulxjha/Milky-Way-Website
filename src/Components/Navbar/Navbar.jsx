525ad28a746e5a973f05b85dd056acaa5e3859f0
rahulxjha <rjrahujha02@gmail.com>
2023-12-07 15:54:06 +0530

    added toggle feature in user ops nav links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,71 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from '../../img/logo.png';
 import '../Navbar/navbar.css';
 
 function Navbar() {
   const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+  const [isSignUpModalOpen, setSignUpModalOpen] = useState(false);
+  const modalOverlayRef = useRef(null);
 
-  const handleAccountClick = (e) => {
-    e.preventDefault();
-    setLoginModalOpen(true);
+  const toggleLoginModal = () => {
+    setSignUpModalOpen(false);
+    setLoginModalOpen((prev) => !prev);
+  };
+
+  const toggleSignUpModal = () => {
+    setLoginModalOpen(false);
+    setSignUpModalOpen((prev) => !prev);
   };
 
   const closeLoginModal = () => {
     setLoginModalOpen(false);
   };
 
+  const closeSignUpModal = () => {
+    setSignUpModalOpen(false);
+  }
+
+  const handleModalOverlayClick = (e) => {
+    if (modalOverlayRef.current && !modalOverlayRef.current.contains(e.target)) {
+      closeLoginModal();
+      closeSignUpModal();
+    }
+  };
+
+  useEffect(() => {
+    if (isLoginModalOpen || isSignUpModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible';
+    }
+
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, [isLoginModalOpen, isSignUpModalOpen]);
+
+  useEffect(() => {
+    document.addEventListener('click', handleModalOverlayClick);
+
+    return () => {
+      document.removeEventListener('click', handleModalOverlayClick);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
-      <React.Fragment>
-        <div className="navbar-left">
-          <img src={logo} alt="Logo" className="logo" />
-        </div>
-        <div className="navbar-center">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/product">Product</a>
-            </li>
-            <li>
-              <a href="/cart">Cart</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-right">
-          <React.Fragment>
-            <a href="/account" onClick={handleAccountClick}>
-              Account
-            </a>
-          </React.Fragment>
-        </div>
-      </React.Fragment>
-
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className="navbar-center">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/product">Product</a>
+          </li>
+          <li>
+            <a href="/cart">Cart</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <a className='user-info' onClick={toggleLoginModal}>
+          Sign in
+        </a>
+        <a className='user-info' onClick={toggleSignUpModal}>
+          Sign up
+        </a>
+      </div>
       {isLoginModalOpen && (
         <div className="modal-overlay">
           <div className="modal">
-            <h2>Login</h2>
-            <form>
-              <label>
-                Email:
-                <input type="text" />
-              </label>
+            <h2>Welcome back!</h2>
+            <form id='login-form'>
+              <label>Email:<input type="text" /></label>
               <br />
-              <label>
-                Password:
-                <input type="password" />
-              </label>
+              <label>Password:<input type="password" /></label>
               <br />
-              <button type="submit">Login</button>
+              <button id="sub-btn" type="submit">Sign in</button>
+              <button id="close-btn" onClick={closeLoginModal}>Close</button>
             </form>
-            <button onClick={closeLoginModal}>Close</button>
           </div>
         </div>
       )}
+
+      {isSignUpModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Welcome! We're excited to have you on board.</h2>
+            <form id='signup-form'>
+              <label>Name : <br /><input type="text" /></label>
+              <br />
+              <label>Phone No. : <br /> <input type="text" /></label>
+              <br />
+              <label>Address : <br /> <input type="text" /></label>
+              <br />
+              <label>Email : <br /> <input type="text" /></label>
+              <br />
+              <label>Password : <br /> <input type="password" /></label>
+              <br />
+              <button id="sub-btn" type="submit">Sign up</button>
+              <button id="close-btn" onClick={closeSignUpModal}>Close</button>
+            </form>
+          </div>
+        </div>
+      )}
+
     </nav>
   );
 }
